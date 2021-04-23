ac09c9156c49f86c1c093ed9ab3acaa71f43724d
NBP77 <nbp77@outlook.com>
2021-04-23 10:07:57 -0400

    started adding tables in model

--- models/workout.js
+++ models/workout.js
@@ -3,16 +3,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutdb = new Schema({
-    
+
     day: {
-      
+        type: Date,
+        default: Date.now
     },
 
     exercises: [                  
         {
-            
-               
-            },
+            type: String,
+            name: String,
+            duration: ,
+            weight: ,
+            reps: ,
+            sets: 
+    },
         
 
     ]
