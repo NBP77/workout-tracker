e8f7a9928631d0973ae780cf30ff9eb096451f26
NBP77 <nbp77@outlook.com>
2021-04-23 22:02:48 -0400

    Succesfully added a PUT and POST

--- routes/api.js
+++ routes/api.js
@@ -4,59 +4,49 @@ const db = require("../models");
 
 // Aggregate, add fields and sum opp
 router.get("/api/workouts", (req, res) => {
-    db.Workout.aggregate( [
-        {
-          $addFields: {
-            totalDuration: { $sum: "$exercises.duration" } ,
-          }
-        },
-     ])
-     .then(dbexercise => {
-        res.json(dbexercise);
-      })
-      .catch(err => {
-        res.json(err);
-      });
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbexercise) => {
+      res.json(dbexercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-// router.put("/api/workouts/:id", (req, res) => {
-//     db.Workout.find({})
-// })
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbexercise) => {
+      res.json(dbexercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// router.post("/api/workouts", (req, res) => {
-//     db.Workout.find({})
-// })
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+    .then((dbexercise) => {
+      res.json(dbexercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+})
 
 // // Aggregate, add fields and sum opp
 // router.get("/api/workouts/range", (req, res) => {
-//     db.Workout.find({})
+//     db.Workout.aggregate({})
 // })
 
 
-
-// app.get("/notes", (req, res) => {
-//     db.Note.find({})
-//       .then(dbNote => {
-//         res.json(dbNote);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
-
-
-
-
-
-
-
 module.exports = router;
-
-
- db.Workout.aggregate( [
-    {
-      $addFields: {
-        totalDuration: { $sum: "$exercises.duration" } ,
-      }
-    },
- ] )
